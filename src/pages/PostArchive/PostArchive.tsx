ddecf19d457d81dca966a7dec6bc19615b881cb4
Aleksandr <oscherniavskii@gmail.com>
2024-06-28 13:27:44 +0300

    Add Load More functionality

--- src/pages/PostArchive/PostArchive.tsx
+++ src/pages/PostArchive/PostArchive.tsx
@@ -1,4 +1,4 @@
-import { type FC } from 'react';
+import { useEffect, useState, type FC } from 'react';
 import {
 	Breadcrumbs,
 	ButtonLink,
@@ -8,31 +8,67 @@ import {
 	Section
 } from '../../components';
 import { LoaderBigCard } from '../../components/loaders';
-import { usePostCards } from '../../hooks/usePostCards';
+import { getAllPostCards } from '../../services/post.services';
+import { PostCard } from '../../types/post.types';
 import './PostArchive.scss';
 
 const PostArchive: FC = () => {
-	const { data, isLoading } = usePostCards('?per_page=7');
+	const perPage = 9;
+	const [offset, setOffset] = useState<number>(1);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const [allPosts, setAllPosts] = useState<PostCard[]>([]);
+	const [firstPost, setFirstPost] = useState<PostCard | undefined>(undefined);
+	const [isFirstLoading, setIsFirstLoading] = useState<boolean>(true);
+	const [allPostsLoaded, setAllPostsLoaded] = useState<boolean>(false);
 
-	const posts = data ? [...data] : [];
-	const firstPost = posts.length > 0 ? posts[0] : undefined;
-	const remainingPosts = posts.slice(1);
+	useEffect(() => {
+		getAllPostCards(`?per_page=1`)
+			.then(data => {
+				if (data && data.length > 0) {
+					setFirstPost(data[0]);
+				}
+			})
+			.catch(error => console.log(error))
+			.finally(() => setIsFirstLoading(false));
+	}, []);
+
+	useEffect(() => {
+		getAllPostCards(`?per_page=${perPage}&offset=${offset}`)
+			.then(data => {
+				if (data && data.length > 0) {
+					setAllPosts(prev => [...prev, ...data]);
+					if (data.length < perPage) {
+						setAllPostsLoaded(true);
+					}
+				} else {
+					setAllPostsLoaded(true);
+				}
+			})
+			.catch(error => console.log(error))
+			.finally(() => setIsLoading(false));
+	}, [offset]);
 
 	return (
 		<>
 			<Section vaiant='top'>
 				<PageTitle title='Все посты' />
 				<Breadcrumbs secondTitle='Все посты' />
-				{isLoading || !data ? (
+				{isFirstLoading || !firstPost ? (
 					<LoaderBigCard />
 				) : (
 					<PostCardBig postData={firstPost} variant='oncover' />
 				)}
 			</Section>
 			<Section>
-				<Catalog posts={remainingPosts} isLoading={isLoading} />
+				<Catalog posts={allPosts} isLoading={isLoading} />
 				<div className='post-archive-action'>
-					<ButtonLink>Загрузить ещё</ButtonLink>
+					{!allPostsLoaded && (
+						<ButtonLink
+							onClick={() => setOffset(prev => prev + perPage)}
+						>
+							Загрузить ещё
+						</ButtonLink>
+					)}
 				</div>
 			</Section>
 		</>
