5fac37d2f7d2714c0bf62553ce29d48008ce07cf
Aleksandr <oscherniavskii@gmail.com>
2024-06-29 20:07:30 +0300

    Some changes to user posts page

--- src/pages/UserPostsPage/UserPostsPage.tsx
+++ src/pages/UserPostsPage/UserPostsPage.tsx
@@ -3,11 +3,10 @@ import { type FC } from 'react';
 import { useParams } from 'react-router-dom';
 import {
 	AuthorInfo,
-	Breadcrumbs,
 	ButtonLink,
 	Catalog,
-	PageTitle,
-	Section
+	Section,
+	SectionTitle
 } from '../../components';
 import { usePostCardsByUser } from '../../hooks/usePostCardsByUser';
 import { useSingleUser } from '../../hooks/useSingleUser';
@@ -21,13 +20,8 @@ const UserPostsPage: FC = () => {
 	return (
 		<>
 			<Section vaiant='top'>
-				<PageTitle title={user?.name} />
-				<Breadcrumbs
-					secondTitle='Авторы'
-					secondLink='/users'
-					thirdTitle={user?.name}
-				/>
 				{!!user && <AuthorInfo user={user} isLoading={isUserLoading} />}
+				<SectionTitle title='Посты автора' />
 				<Catalog posts={data} isLoading={isLoading} />
 				<div className='button-block'>
 					<ButtonLink to='/posts'>Все посты</ButtonLink>
