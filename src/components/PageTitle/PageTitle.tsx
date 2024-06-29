ac5dd9d598c8d86c62499d21ef35d25d46fae458
Aleksandr <oscherniavskii@gmail.com>
2024-06-29 14:06:38 +0300

    Change page title

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -2,11 +2,11 @@ import { type FC } from 'react';
 import './PageTitle.scss';
 
 interface PageTitleProps {
-	title: string;
+	title: string | undefined;
 }
 
 const PageTitle: FC<PageTitleProps> = ({ title }) => {
-	return <h1 className='page-title'>{title}</h1>;
+	return <h1 className='page-title'>{title ? title : ''}</h1>;
 };
 
 export default PageTitle;
